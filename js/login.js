d08fac4db9cab8a809c526d6fd71cb6073a49751
dpqls0356 <dpqls0356@gmail.com>
2022-11-30 14:08:03 +0900

    로그인 js 수정
    유저정보가 없을 경우에 버튼 리스너가 발동할 수 있도록함
    새로운 유저가 값을 입력한 경우 localStorage에 값을 저장할 수 있도록 구현

--- js/login.js
+++ js/login.js
@@ -7,33 +7,26 @@ var userpasswd;
 const welcomemessage = document.querySelector(".welcomemgs");
 const HIDDENCLASS = "hidden";
 
-//localStorage -> 브라우저에 값을 저장할 수 있는 api
-//getItem() removeItem() setItem()
-
+//user정보확인
 username = localStorage.getItem("username");
 if (username === null) {
   loginform.classList.remove(HIDDENCLASS);
   welcomemessage.classList.add(HIDDENCLASS);
+  loginbtn.addEventListener("click", handleLoginClick);
 } else {
-  printWelcomMessage();
+  printWelcomMessage(username);
 }
 
-// functionname() -> 이 코드를 보자마자 함수가 실행돰
-//  functionname -> 브라우저가 이벤트 발생시에 함수 실행됨
-
-//form 의 기본동작은 전송 -> submit
-//link의 기본동작은 page 이동 ->click
-
 //로그인 버튼을 누른 경우
-loginbtn.addEventListener("click", handleLoginClick);
-// 브라우저는 이벤트 발생 시 브라우저내에서 이벤트와 관련된 정보를 가져와 함수를 실행시킴 (첫번째 인자값으로 지정)
 function handleLoginClick(event) {
   event.preventDefault();
   username = userinputname.value;
   userpasswd = userinputpasswd.value;
-  printWelcomMessage();
+  localStorage.setItem("username", username);
+  printWelcomMessage(username);
 }
 
+//유저명과 환영인사 출력
 function printWelcomMessage(username) {
   loginform.classList.add(HIDDENCLASS);
   welcomemessage.classList.remove(HIDDENCLASS);

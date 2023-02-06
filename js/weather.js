4b626fe72964a9b295a36ada43d658ec72fc60a3
dpqls0356 <dpqls0356@gmail.com>
2023-02-06 21:56:36 +0900

    현재 지역의 날씨를 볼 수 있도록 함
    open weather api에서 키를 다운받아 apikey에 넣어주면 사용가능
    css완료

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,5 @@
-
 const APIKEY = "";
 const weatherinfobox = document.querySelector(".weather");
-
 
 navigator.geolocation.getCurrentPosition(geosuccess, geoerror);
 
@@ -12,14 +10,40 @@ function geosuccess(position) {
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
+      const local = data.name;
       const weather = data.weather[0].main;
       const temp = data.main.temp;
+      const weatherIcon = data.weather[0].icon;
+
+      const weatherIconImg = document.createElement("img");
+      const weatherIconAdrs = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
+      weatherIconImg.setAttribute("src", weatherIconAdrs);
+      weatherIconImg.classList.add("weatherIconImg");
       const weatherbox = document.createElement("div");
       weatherbox.innerHTML = `${weather}`;
+
       const tempbox = document.createElement("div");
-      tempbox.innerHTML = `${temp}`;
-      weatherinfobox.appendChild(tempbox);
-      weatherinfobox.appendChild(weatherbox);
+      tempbox.innerHTML = `${temp}°C`;
+
+      const localbox = document.createElement("div");
+      localbox.innerHTML = `${local}`;
+
+      const weatherboxtop = document.createElement("div");
+      weatherboxtop.classList.add("weathertop");
+      const weatherboxbottom = document.createElement("div");
+      weatherboxbottom.classList.add("weatherbottom");
+
+      localbox.classList.add("local");
+      tempbox.classList.add("temp");
+      weatherbox.classList.add("weatherword");
+
+      weatherboxtop.appendChild(localbox);
+      weatherboxtop.appendChild(tempbox);
+      weatherboxbottom.appendChild(weatherbox);
+      weatherboxbottom.appendChild(weatherIconImg);
+
+      weatherinfobox.appendChild(weatherboxtop);
+      weatherinfobox.appendChild(weatherboxbottom);
     });
 }
 function geoerror() {

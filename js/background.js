fc77a22369c5254e51c168bb9ac8ea8b3d170660
dpqls0356 <dpqls0356@gmail.com>
2023-04-17 15:22:42 +0900

    배경 고정 + 더보기 Part 애니메이션 추가
    배경 변경 애니메이션이 생각보다 부자연스러워서 하나로 고정 - > 후에 동영상 배경으로 넣을 예정
    
    더보기 part가 나타나고 사라지는 것이 부자연스러워 자연스럽게 나타나도록 수정

--- js/background.js
+++ js/background.js
@@ -1,29 +1,29 @@
-const backgroundbox = document.querySelector(".background");
-const backgroundimg = document.createElement("img");
-const prebackgroundimg = document.querySelector(".prebackground img")
-var preindex=1;
-var backgroundimgnumber
-const imgcount = 5;
-setInterval(changeBackgroudImg, 10000);
-function changeBackgroudImg() {
-  while(true){
-    backgroundimgnumber = parseInt(Math.random() * imgcount + 1);
-    if(preindex==backgroundimgnumber)continue;
-    else {
-      preindex=backgroundimgnumber;
-      break;
-    }
-  }
-  backgroundimg.src = `/img/background${backgroundimgnumber}.jpg`;
-  prebackgroundimg.classList.remove("remove-prebackground");
-  void prebackgroundimg.offsetWidth;
-  prebackgroundimg.classList.add("remove-prebackground");
-  backgroundimg.style.animation="showBackground 2s cubic-bezier(0.250, 0.460, 0.450, 0.940) both";
-  backgroundbox.appendChild(backgroundimg);
-  setTimeout(() => {
-    prebackgroundimg.src = `/img/background${backgroundimgnumber}.jpg`;
-  }, 4000);
-}
+// const backgroundbox = document.querySelector(".background");
+// const backgroundimg = document.createElement("img");
+// const prebackgroundimg = document.querySelector(".prebackground img")
+// var preindex=1;
+// var backgroundimgnumber
+// const imgcount = 5;
+// setInterval(changeBackgroudImg, 10000);
+// function changeBackgroudImg() {
+//   while(true){
+//     backgroundimgnumber = parseInt(Math.random() * imgcount + 1);
+//     if(preindex==backgroundimgnumber)continue;
+//     else {
+//       preindex=backgroundimgnumber;
+//       break;
+//     }
+//   }
+//   backgroundimg.src = `/img/background${backgroundimgnumber}.jpg`;
+//   prebackgroundimg.classList.remove("remove-prebackground");
+//   void prebackgroundimg.offsetWidth;
+//   prebackgroundimg.classList.add("remove-prebackground");
+//   backgroundimg.style.animation="showBackground 2s cubic-bezier(0.250, 0.460, 0.450, 0.940) both";
+//   backgroundbox.appendChild(backgroundimg);
+//   setTimeout(() => {
+//     prebackgroundimg.src = `/img/background${backgroundimgnumber}.jpg`;
+//   }, 4000);
+// }
 
 
 

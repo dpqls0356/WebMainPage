a1c055d83beb9b5a0374a57edf57e2c2ccd67553
dpqls0356 <dpqls0356@gmail.com>
2023-04-17 14:47:42 +0900

    배경 이미지 변경 애니메이션 추가

--- js/background.js
+++ js/background.js
@@ -1,12 +1,30 @@
 const backgroundbox = document.querySelector(".background");
 const backgroundimg = document.createElement("img");
+const prebackgroundimg = document.querySelector(".prebackground img")
+var preindex=1;
+var backgroundimgnumber
 const imgcount = 5;
-
-changeBackgroudImg();
 setInterval(changeBackgroudImg, 10000);
 function changeBackgroudImg() {
-  const backgroundimgnumber = parseInt(Math.random() * imgcount + 1);
+  while(true){
+    backgroundimgnumber = parseInt(Math.random() * imgcount + 1);
+    if(preindex==backgroundimgnumber)continue;
+    else {
+      preindex=backgroundimgnumber;
+      break;
+    }
+  }
   backgroundimg.src = `/img/background${backgroundimgnumber}.jpg`;
-
+  prebackgroundimg.classList.remove("remove-prebackground");
+  void prebackgroundimg.offsetWidth;
+  prebackgroundimg.classList.add("remove-prebackground");
+  backgroundimg.style.animation="showBackground 2s cubic-bezier(0.250, 0.460, 0.450, 0.940) both";
   backgroundbox.appendChild(backgroundimg);
+  setTimeout(() => {
+    prebackgroundimg.src = `/img/background${backgroundimgnumber}.jpg`;
+  }, 4000);
 }
+
+
+
+// background를 미리 하나 깔아놓고 일정시간마다 background를 추가해주기
